0da75293fc98813347d0e3092de6f2a82e9d78e8
Mark Mauro <markymauro@gmail.com>
2020-09-16 15:03:09 -0400

    finished the rest of the program

--- convert.py
+++ convert.py
@@ -5,3 +5,24 @@ def ftToIn(ft):
 def inToFt(inch):
     inch //=12
     return inch
+
+i = 0
+
+while(i != 1):
+    m = int(input("Enter 0 for ft to inches OR Enter 1 for in to ft: "))
+    print()
+
+    if(m==0):
+        feet = int(input("Enter number in feet: "))
+        print()
+        print(ftToIn(feet))
+        print()
+    elif(m==1):
+        inch = int(input("Enter number in inches: "))
+        print()
+        print(inToFt(inch))
+        print()
+    else:
+        print("invalid input. Enter a number between 0-1")
+        print()
+    
